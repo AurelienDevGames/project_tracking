76ca4e21f30d05547bd8f877297028905c2e49be
CptNapalm <mwliad@gmail.com>
2016-09-18 14:32:51 +0200

    It's alive

---
+++ ThoughtBubbles/EFContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ThoughtBubbles.Models;
+
+namespace ThoughtBubbles
+{
+    public class MotivationContext : System.Data.Entity.DbContext
+    {
+        public DbSet<Motivation> Motivation { get; set; }
+        public DbSet<Project> Project { get; set; }
+        public DbSet<Question> Question { get; set; }
+    }
+}
---
+++ ThoughtBubbles/Models/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ThoughtBubbles.Models
+{
+    public class Question
+    {
+
+        [Key]
+        public int QuestionId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string QuestionText { get; set; }
+        public string AnswerText { get; set; }
+
+        public virtual Project Project { get; set; }
+    }
+}
