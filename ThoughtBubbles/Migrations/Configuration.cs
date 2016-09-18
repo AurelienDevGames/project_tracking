b093d5a6f9c71964d3a843799146249b247c78cd
CptNapalm <mwliad@gmail.com>
2016-09-18 15:41:04 +0200

    Automatic migration enabled

--- ThoughtBubbles/Migrations/Configuration.cs
+++ ThoughtBubbles/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace ThoughtBubbles.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(ThoughtBubbles.MotivationContext context)
