1c85958a76248cc7ffdb6890aafdbef12166f4ef
Aurelien <ext.modis.aurelien.gounot@sncf.fr>
2016-09-22 09:32:30 +0200

    Bundling scripts & styles to optimize page load

---
+++ ThoughtBubbles/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ThoughtBubbles.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/vendor").IncludeDirectory("~/Scripts", "*.js"));
+            bundles.Add(new StyleBundle("~/bundles/style").IncludeDirectory("~/Content", "*.css"));
+            BundleTable.EnableOptimizations = true;
+
+        }
+    }
+}
