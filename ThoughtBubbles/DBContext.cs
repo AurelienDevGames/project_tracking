2116f0811ac6286afba67cbb6bd738ad025132cf
Aurelien <ext.modis.aurelien.gounot@sncf.fr>
2016-09-26 15:47:50 +0200

    Switch DB using MODE PRAGMA

--- ThoughtBubbles/DBContext.cs
+++ ThoughtBubbles/DBContext.cs
@@ -15,16 +15,20 @@ namespace ThoughtBubbles
 
         public DBContext()
         {
-            Factory = new OrmLiteConnectionFactory(Dbswitcher(), SqlServerDialect.Provider);
             isPROD = true;
+#if DEBUG
+            isPROD = false;
+#endif
+            Factory = new OrmLiteConnectionFactory(Dbswitcher(), SqlServerDialect.Provider);
         }
 
         private string Dbswitcher()
         {
             return isPROD ?
-                "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ThoughtBubbles.MotivationContext;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
-                : "Server=tcp:4thsanctuary.database.windows.net,1433;Initial Catalog=SQL_4thSanctuary;Persist Security Info=False;User ID=codfractal;Password=ADTuECOyY3t0;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-            ;
+                 "Server=tcp:4thsanctuary.database.windows.net,1433;Initial Catalog=SQL_4thSanctuary;Persist Security Info=False;User ID=codfractal;Password=ADTuECOyY3t0;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+                  : "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ThoughtBubbles.MotivationContext;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+                 ;
+
         }
 
     }
